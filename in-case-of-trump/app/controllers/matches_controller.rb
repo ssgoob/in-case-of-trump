9388ad0dac165f379892c834d42e0f5a5af2d4de
chintas1 <chintas1@tcnj.edu>
2016-04-07 17:59:59 -0400

    matches model

--- in-case-of-trump/app/controllers/matches_controller.rb
+++ in-case-of-trump/app/controllers/matches_controller.rb
@@ -1,2 +1,17 @@
 class MatchesController < ApplicationController
+  
+  def index 
+    #get matches for current_user 
+    @matches = Matches.all
+  end
+
+  # def new
+  # end
+
+  # def create
+  # end
+
+  def show
+    @match = Match.find(params[:id])
+  end
 end
--- in-case-of-trump/app/controllers/users_controller.rb
+++ in-case-of-trump/app/controllers/users_controller.rb
@@ -6,18 +6,33 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-  end
 
-  def show
   end
 
   def create
+    @user = User.new(user_params)
+
+    if(@user.save)
+        if(params[:status]=='Citizen')
+          #make citizen and assign this user to citizen
+        else
+          #make international and assign this user to international
+        end
+      redirect @user
+    else
+      render :new
+    end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :gender, :dob, :location, :preference)
+    params.require(:user).permit(:name, :gender, :dob, :preference)
   end
 
 

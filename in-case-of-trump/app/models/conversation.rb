29435725adf5ae36dce2451ed3dd292affec7f68
SaiedAbbasi <saiedabbasijr@gmail.com>
2016-04-12 15:40:59 -0400

    conversation view

--- in-case-of-trump/app/models/conversation.rb
+++ in-case-of-trump/app/models/conversation.rb
@@ -12,20 +12,21 @@ class Conversation < ActiveRecord::Base
   belongs_to :match
   has_many :messages
 
-  # if Match status is 'success', create conversation using match_id
+  
   def create_conversation
     Match.where(status == "Matched")
     Conversation.new()
   end
 
-# 1. If a user makes a match and that match's status is "Matched"
-# 2. Create a new conversation with a match id that is same as the match's id
-# 3. Return a conversation
-
-
-  # match.create_conversation
-  #   Match.where(status == "Matched")
-  #   Conversation.new(match_id = self.id)    
+  def has_access?(current_user)
+    if current_user.status == 'Citizen' && current_user.status_id == self.match.citizen_id 
+      true
+    elsif current_user.status == 'International' && current_user.status_id == self.match.international_id 
+      true
+    else
+      false
+    end  
+  end
 
 
 end

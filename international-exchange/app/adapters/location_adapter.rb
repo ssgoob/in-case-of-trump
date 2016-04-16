1a2ada367356ea15b99c6e1ce0d22254e60fdfbd
samnags <sam.nagourney@gmail.com>
2016-04-16 15:21:51 -0400

    api back

--- international-exchange/app/adapters/location_adapter.rb
+++ international-exchange/app/adapters/location_adapter.rb
@@ -1,20 +1,20 @@
 class LocationAdapter
 
-  attr_reader :latitude, :longitude
+  attr_reader :latitude, :longitude, :radius
 
-  def initialize(latitude, longitude)
+  def initialize(latitude, longitude, radius = 50)
     @latitude = latitude
     @longitude = longitude
+    @radius = radius
   end
 
   def response
-    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?&type=restaurant&location=#{latitude},#{longitude}&radius=50&key=AIzaSyBvQNt2nQGr5R1yyAB8_ymfyb_i6hDxtmo"
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?&type=restaurant&location=#{latitude},#{longitude}&radius=#{radius}&key=AIzaSyBvQNt2nQGr5R1yyAB8_ymfyb_i6hDxtmo"
     response = HTTParty.get(url) #makes the request
   end
 
   def response_data
-    binding.pry
-    response['yelp_first_key']
+    response['results']
   end  
 
 end  
--- international-exchange/app/adapters/location_client.rb
+++ international-exchange/app/adapters/location_client.rb
@@ -1,20 +1,46 @@
 class LocationClient
 
-  attr_reader :query, :connection
+  
+  attr_reader :latitude, :longitude, :radius
 
-  def initialize
-    @query = query
-    @connection ||= LocationConnection.new(query)
+  def initialize(latitude, longitude, radius = 50)
+    @latitude = latitude
+    @longitude = longitude
+    @radius = radius
+    @connection ||= LocationAdapter.new(latitude, longitude, radius)
   end
 
-  def create_recommendations
-    connection.response_data.map do |rec|
-      dining = rec['rating']
-      parks = rec['green_stuff']
-      city = rec['name']
-      # weather...
-      Location.create(dining: dining, parks: parks, city: city)
+# an array of objects or hashes
+  def recommendations
+    @recommendations = []
+    @connection.response_data.map do |rec|
+      recommendation = {}
+      recommendation['name'] = rec["name"]
+      recommendation['rating'] = rec["rating"] 
+      recommendation['price'] = rec["price_level"]
+      recommendation['address'] = rec["vicinity"]
+      @recommendations << recommendation
     end
+    @recommendations
   end
-
+  
+  
 end  
+
+
+
+
+# test = LocationClient.new(40.685722, -73.952905)
+# test = LocationClient.new(40.685722, -73.952905, 500)
+
+
+
+ # @latitude=43.97682496107221,
+ #   @longitude=27.84500718141999,
+ #   @radius=1000000>,
+ # url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?&type=restaurant&location=43.97682496107221,27.84500718141999&radius=10000&key=AIzaSyBvQNt2nQGr5R1yyAB8_ymfyb_i6hDxtmo"
+
+
+ # @latitude=46.39918096766364,
+ #   @longitude=30.06804804829307,
+ #   @radius=10000>,

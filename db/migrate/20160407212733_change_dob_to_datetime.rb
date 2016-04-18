d07e443a1a0985e0e049ff590a03afaf6b44ffd1
chintas1 <chintas1@tcnj.edu>
2016-04-18 11:36:43 -0400

    db issues

--- db/migrate/20160407212733_change_dob_to_datetime.rb
+++ db/migrate/20160407212733_change_dob_to_datetime.rb
@@ -1,5 +1,5 @@
 class ChangeDobToDatetime < ActiveRecord::Migration
   def change
-    change_column :users, :dob, :timestamp
+    change_column :users, :dob, "USING dob::datetime without time zone"
   end
 end

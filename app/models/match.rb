b0f2b1505272fa0a9406b607c0f1ebda948290b6
chintas1 <chintas1@tcnj.edu>
2016-04-18 11:27:07 -0400

    db compatibility issues

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,7 +1,6 @@
 class Match < ActiveRecord::Base
   belongs_to :citizen
   belongs_to :international
-  has_one :conversation
 
   def self.conversing_users
     citizen_count = Citizen.joins(:matches).where('matches.status = "Matched"').group('citizens.id').count 
